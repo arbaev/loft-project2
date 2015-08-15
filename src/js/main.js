3632ad1ea2a4d18d2762099001130e3a599b554c
Tim Arbaev <timbrk@gmail.com>
2015-08-15 12:42:59 +0300

    Функция hover у корзины

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
-console.log('all ok');
+'use strict'
+
+var shopModule = (function() {
+	// инициализация функций
+	var init = function () {
+		_setupListeners();
+	};
+
+	// прослушка событий
+	var _setupListeners = function () {
+		$('.header__basket').hover(_showBasket); // показать содержимое корзины
+	};
+
+	// показать модальное окно добавления проекта
+	var _showBasket = function(ev) {
+		// ev.preventDefault();
+		$('.basket').toggle();
+	};
+
+
+
+	return {
+		init : init
+	};
+
+})();
+
+shopModule.init();
